242e47d80cdba4d13cb12dc02d26a7362600cc61
Son Mac <mactranthienson@gmail.com>
2022-11-16 01:12:18 -0700

    Create route for writing studies to db

---
+++ pages/api/create-survey.js
@@ -0,0 +1,31 @@
+import mongoClientPromise from '../../core/db/mongoClient';
+
+const handler = async (req, res) => {
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' });
+    return;
+  }
+  const mongoClient = await mongoClientPromise;
+  const { researcherID, participantID, studyData } = req.body;
+
+  try {
+    mongoClient.db().collection('studies').insertOne({
+      timestamp: new Date(),
+      createdBy: researcherID,
+      assignedTo: participantID,
+      study: studyData,
+    });
+    res.status(200).send({
+      success: true,
+      message: 'The study has been saved!',
+    });
+  } catch (err) {
+    res.status(400).send({
+      success: false,
+      message: 'An error happened while saving the study. Please try again!',
+      error: err,
+    });
+  }
+};
+
+export default handler;
