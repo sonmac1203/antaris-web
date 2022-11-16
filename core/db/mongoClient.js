ecbba42472f1c4e2a1cb16b7120095a82931bdf6
Son Mac <mactranthienson@gmail.com>
2022-11-16 14:16:24 -0700

    Fix small typo

--- core/db/mongoClient.js
+++ core/db/mongoClient.js
@@ -3,7 +3,7 @@ require('custom-env').env();
 
 const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
-const databaseName = process.env.DBNAME;
+const databaseName = process.env.MONGO_DBNAME;
 
 const mongoURI = `mongodb+srv://${username}:${password}@cluster0.haay1m9.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
 
