c16de643b2a57c5fd9a76f20a2c5dbcd846d6546
Son Mac <mactranthienson@gmail.com>
2023-03-03 02:04:40 -0700

    Update Dash - Allow the page to refresh the access token when it expires

--- core/utils/mdh/get-access-token.js
+++ core/utils/mdh/get-access-token.js
@@ -3,18 +3,15 @@ import jwt from 'jwt-simple';
 
 const secret = 'antarissecret';
 
-export const getAccessToken = async (serviceAccountId, projectId, auth) => {
+export const getAccessToken = async (serviceAccountId, projectId) => {
   try {
     const { data } = await axios.get('/api/mdh/get_access_token', {
       params: { serviceAccountId: serviceAccountId, projectId: projectId },
     });
 
     if (data.success) {
-      if (auth) {
-        const auth_token = jwt.encode({ mdh_id: serviceAccountId }, secret);
-        return { ...data, auth_token };
-      }
-      return data;
+      const auth_token = jwt.encode({ mdh_id: serviceAccountId }, secret);
+      return { ...data, auth_token };
     }
     return data;
   } catch (error) {
--- pages/dash.jsx
+++ pages/dash.jsx
@@ -3,20 +3,25 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import { Dash } from '../components/dash';
 import { DashboardLayout } from '@/components/layouts';
+import { getAccessToken } from '@/core/utils/mdh';
 
 const DashPage = () => {
   const router = useRouter();
   const [accountData, setAccountData] = useState();
+  const [timeUntilTokenExpiration, setTimeUntilTokenExpiration] =
+    useState(null);
 
   useEffect(() => {
-    if (!localStorage.getItem('auth_token')) {
+    const authToken = localStorage.getItem('auth_token');
+    if (!authToken) {
       router.push('/');
       return;
     }
-    (async () => {
+    // Fetch the user's account data and set it in state
+    const fetchAccountData = async () => {
       try {
         const { data } = await axios.get('/api/auth/new_auth', {
-          headers: { 'x-auth': localStorage.getItem('auth_token') },
+          headers: { 'x-auth': authToken },
         });
         if (data.success) {
           setAccountData(data.accountData);
@@ -26,10 +31,57 @@ const DashPage = () => {
       } catch (err) {
         router.push('/');
       }
-    })();
+    };
+    fetchAccountData();
   }, []);
 
-  return accountData && <Dash data={accountData} />;
+  useEffect(() => {
+    if (!accountData) {
+      return;
+    }
+    const { token_expires_at } = accountData;
+    const timeUntilExpiration = Math.ceil(
+      (new Date(token_expires_at).getTime() - Date.now()) / 1000
+    );
+    setTimeUntilTokenExpiration(timeUntilExpiration);
+    const intervalId = setInterval(() => {
+      setTimeUntilTokenExpiration((prevTime) => {
+        const newTime = prevTime - 1;
+        if (newTime >= 0) {
+          return newTime;
+        } else {
+          // If the time until expiration reaches 0, get a new access token
+          clearInterval(intervalId);
+          refreshAccessToken();
+          return 0;
+        }
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [accountData]);
+
+  const refreshAccessToken = async () => {
+    const { mdh_id: serviceAccountId, project_id: projectId } = accountData;
+    const apiResponse = await getAccessToken(serviceAccountId, projectId);
+    if (apiResponse.success) {
+      localStorage.setItem('auth_token', apiResponse.auth_token);
+      router.reload();
+    } else {
+      router.push('/');
+    }
+  };
+
+  return (
+    accountData && (
+      <>
+        The token will expire in {timeUntilTokenExpiration} seconds
+        <Dash data={accountData} />
+      </>
+    )
+  );
 };
 
 DashPage.Layout = DashboardLayout;
