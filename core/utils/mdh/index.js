2ec486706c4286aab977293120cd6a8daae70da8
Son Mac <mactranthienson@gmail.com>
2023-03-23 02:45:14 -0700

    Refinement: Remove unused mdh routes and utils

--- core/utils/mdh/index.js
+++ core/utils/mdh/index.js
@@ -1,4 +1,2 @@
-export { getFromApi } from './get-from-api';
-export { getAccessToken } from './get-access-token';
 export { fetchMdhSurveys } from './fetch-mdh-surveys';
 export { fetchMdhParticipants } from './fetch-mdh-participants';
