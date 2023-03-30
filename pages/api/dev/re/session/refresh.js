f75c19b071a443e09bec5431cf9b462c266f8285
Son Mac <mactranthienson@gmail.com>
2023-03-30 04:16:00 -0700

    (Miss) Add /api/dev/re/session

---
+++ pages/api/dev/re/session/refresh.js
@@ -0,0 +1,64 @@
+import ServiceAccount from '@/core/models/ServiceAccount';
+import {
+  withSessionApiRoute,
+  getExpirationTimestamp,
+  getMdhAccessToken,
+  jwtUtils,
+} from '@/core/utils';
+
+const handler = async (req, res) => {
+  const { token } = req.session;
+
+  if (!token) {
+    return res.status(403).json({
+      message: 'You are not authorized',
+    });
+  }
+
+  const { projectId, serviceAccountId } = jwtUtils.decode(token);
+  const { redirect } = req.query;
+
+  try {
+    const existingAccount = await ServiceAccount.findOne({
+      mdh_id: serviceAccountId,
+      project_id: projectId,
+    });
+
+    const { privateKey } = jwtUtils.decode(existingAccount.private_key_encoded);
+
+    const result = await getMdhAccessToken(
+      serviceAccountId,
+      projectId,
+      privateKey
+    );
+
+    if (!result) {
+      req.session.destroy();
+      await req.session.save();
+      return res.status(307).redirect('/');
+    }
+
+    const tokenExpiresAt = getExpirationTimestamp(result.expires_in);
+
+    existingAccount.access_token = result.access_token;
+    existingAccount.token_expires_at = tokenExpiresAt;
+    await existingAccount.save();
+
+    const itemToEncode = {
+      serviceAccountId,
+      accessToken: result.access_token,
+      projectId,
+      tokenExpiresAt,
+    };
+    const authToken = jwtUtils.encode(itemToEncode);
+    req.session.token = authToken;
+    await req.session.save();
+    return res.status(307).redirect(redirect);
+  } catch (err) {
+    req.session.destroy();
+    await req.session.save();
+    return res.status(307).redirect('/');
+  }
+};
+
+export default withSessionApiRoute(handler);
