f2f1c695fa5aacc03b87d4988b8a9c5ee7a0b6b6
Son Mac <mactranthienson@gmail.com>
2023-04-01 11:54:13 -0700

    Update send route and useSurvey hook

--- core/hooks/useSurvey.jsx
+++ core/hooks/useSurvey.jsx
@@ -3,38 +3,62 @@ import { SurveyContext } from '../context';
 import axios from 'axios';
 
 export const useSurvey = () => {
-  const [surveyContent, setSurveyContent] = useState(null);
-  const [selectedParticipants, setSelectedParticipants] = useState(null);
-
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
 
   const surveyContext = useContext(SurveyContext);
-
   const { surveyData } = surveyContext;
 
-  const sendSurvey = useCallback(async () => {
-    const requestBody = {
-      participantIds: selectedParticipants,
-      surveyContent,
-      surveyData,
-    };
-    try {
-      setLoading(true);
-      await axios.post(`/api/surveys/${surveyData.surveyID}/send`, requestBody);
-    } catch (err) {
-      setError(err.response.data);
-    } finally {
-      setLoading(false);
-    }
-  }, [surveyData, surveyContent]);
+  const sendSurvey = useCallback(
+    async (ids) => {
+      const requestBody = {
+        participantIds: ids,
+      };
+      try {
+        setLoading(true);
+        await axios.post(
+          `/api/dev/re/surveys/${surveyData.surveyID}/send`,
+          requestBody
+        );
+        setSuccess(true);
+      } catch (err) {
+        setError(err.response.data);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [surveyData]
+  );
+
+  const saveSurvey = useCallback(
+    async (surveyQuestions) => {
+      const requestBody = {
+        surveyQuestions,
+        surveyData,
+      };
+      try {
+        setLoading(true);
+        await axios.post(
+          `/api/dev/re/surveys/${surveyData.surveyID}/save`,
+          requestBody
+        );
+        setSuccess(true);
+      } catch (err) {
+        setError(err.response.data);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [surveyData]
+  );
 
   return {
     error,
     loading,
-    ...surveyContext,
-    setSurveyContent,
-    setSelectedParticipants,
+    success,
     sendSurvey,
+    saveSurvey,
+    ...surveyContext,
   };
 };
---
+++ pages/api/dev/re/surveys/[survey_id]/send.js
@@ -0,0 +1,55 @@
+import { withSessionApiRoute } from '@/core/utils/session';
+import Survey from '@/core/models/Survey';
+import Participant from '@/core/models/Participant';
+
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Only POST requests allowed' });
+  }
+
+  const { token } = req.session;
+  if (!token) {
+    return res.status(403).json({
+      success: false,
+      message: 'You are not authorized to make this request.',
+    });
+  }
+
+  const { survey_id } = req.query;
+  const { participantIds } = req.body;
+
+  try {
+    const existingSurvey = await Survey.findOne({ mdh_id: survey_id });
+    const participants = await Participant.find({
+      participant_identifier: { $in: participantIds },
+    });
+
+    existingSurvey.assigned_to = participants.map((p) => ({
+      participant: p._id,
+    }));
+
+    const promisesToUpdateParticipants = participants.map(
+      async (participant) => {
+        participant.alexa_metadata.assigned_surveys.push({
+          survey: existingSurvey._id,
+        });
+        await participant.save();
+      }
+    );
+    await existingSurvey.save();
+    await Promise.all(promisesToUpdateParticipants);
+
+    return res.status(200).json({
+      success: true,
+      message: 'The survey is created and participants are updated.',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: 'There was an error. We cannot fetch survey data.',
+      error: error.message,
+    });
+  }
+}
+
+export default withSessionApiRoute(handler);
