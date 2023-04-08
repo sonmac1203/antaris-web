e52ea102de08b0962453d54adc2383cbe56570ca
Son Mac <mactranthienson@gmail.com>
2023-04-08 09:15:59 -0700

    Update PageSectionWrapper

--- core/components/PageSectionWrapper/PageSectionWrapper.jsx
+++ core/components/PageSectionWrapper/PageSectionWrapper.jsx
@@ -1,7 +1,10 @@
-export const PageSectionWrapper = ({ children, title }) => {
+export const PageSectionWrapper = ({ children, title, topRightOptions }) => {
   return (
     <div className='d-flex flex-column gap-2 w-100'>
-      <h2 className='fs-5 text-secondary'>{title}</h2>
+      <div className='d-flex align-items-center justify-content-between mb-1'>
+        <h2 className='fs-5 text-secondary mb-0'>{title}</h2>
+        {!!topRightOptions && topRightOptions}
+      </div>
       {children}
     </div>
   );
