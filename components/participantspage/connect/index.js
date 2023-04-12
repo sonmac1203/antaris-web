d3dc03c10911a7ce9b583e0a11050aedb19f4657
Son Mac <mactranthienson@gmail.com>
2023-04-12 00:19:01 -0700

    Clean-up old auth routes (still leave signout route)

--- components/participantspage/connect/index.js
+++
@@ -1,1 +0,0 @@
-export { ConnectAlexaPage } from './ConnectAlexaPage';
--- components/participantspage/landing/ParticipantsPage.jsx
+++
@@ -1,9 +0,0 @@
-import { LoginWithAmazonButton } from '@/core/components';
-
-export const ParticipantsPage = () => {
-  return (
-    <div>
-      <LoginWithAmazonButton />
-    </div>
-  );
-};
