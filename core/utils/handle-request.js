20ff06abb96f0fb67745bfd4c3a567f825880009
Son Mac <mactranthienson@gmail.com>
2023-04-12 00:34:30 -0700

    Remove unused utils - getRegexFromPaths - handleRequest

--- core/utils/handle-request.js
+++
@@ -1,8 +0,0 @@
-export async function handleRequest(request) {
-  try {
-    const response = await request();
-    return response.data;
-  } catch (error) {
-    return error.response.data;
-  }
-}
