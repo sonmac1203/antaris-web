4400e0986a750dd123af65d97775847d35074e93
Son Mac <mactranthienson@gmail.com>
2023-04-11 23:39:39 -0700

    Clean-up: remove home components

--- components/homepage/components/ProjectIdInput/index.js
+++
@@ -1,1 +0,0 @@
-export { ProjectIdInput } from './ProjectIdInput';
--- components/homepage/components/index.js
+++
@@ -1,4 +0,0 @@
-export { InputArea } from './InputArea';
-export { ProjectIdInput } from './ProjectIdInput';
-export { ServiceAccountIdInput } from './ServiceAccountIdInput';
-export { ConnectButton } from './ConnectButton';
