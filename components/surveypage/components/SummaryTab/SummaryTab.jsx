47e36720ab27d1b9f9dce60bd2bdadc27016616c
Son Mac <mactranthienson@gmail.com>
2023-04-16 19:51:09 -0700

    Guard SummaryTab in no data case

--- components/surveypage/components/SummaryTab/SummaryTab.jsx
+++ components/surveypage/components/SummaryTab/SummaryTab.jsx
@@ -5,17 +5,27 @@ import {
 } from './components';
 import { memo } from 'react';
 import styles from './SummaryTab.module.css';
+import { useSurvey } from '@/lib/re/surveyoverview';
 
 export const SummaryTab = memo(() => {
+  const { surveyData } = useSurvey();
+  const { assigned_to: assignments } = surveyData;
+
   return (
     <div className='d-flex flex-column gap-4 mb-5'>
-      <div className={`d-flex flex-wrap align-items-stretch ${styles.Row}`}>
-        <StatusDonutChart />
-        <AssignmentProgress />
-      </div>
-      <div>
-        <QuestionList />
-      </div>
+      {!assignments ? (
+        'No assignment found.'
+      ) : (
+        <>
+          <div className={`d-flex flex-wrap align-items-stretch ${styles.Row}`}>
+            <StatusDonutChart />
+            <AssignmentProgress />
+          </div>
+          <div>
+            <QuestionList />
+          </div>
+        </>
+      )}
     </div>
   );
 });
