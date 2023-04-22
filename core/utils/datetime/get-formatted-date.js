a6b0b400b21a35d3ae9953824401d6168e9d7c5d
Son Mac <mactranthienson@gmail.com>
2023-04-22 02:56:55 -0700

    Update getFormattedDate

--- core/utils/datetime/get-formatted-date.js
+++ core/utils/datetime/get-formatted-date.js
@@ -1,7 +1,9 @@
-export const getFormattedDate = (date) =>
-  date.toLocaleString('en-US', {
+export const getFormattedDate = (date) => {
+  const thisDate = new Date(date);
+  return thisDate.toLocaleString('en-US', {
     hour: 'numeric',
     minute: 'numeric',
     day: 'numeric',
     month: 'long',
   });
+};
