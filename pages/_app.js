683d4ff110b897fa503e709ab437fc00807ee50a
Son Mac <mactranthienson@gmail.com>
2023-04-22 17:02:54 -0700

    Add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,43 @@
 import '../styles/globals.css';
+import '../styles/nprogress.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { useEffect } from 'react';
 import { HomeLayout } from '../components/layouts';
-import { AppContextProvider, SessionRefreshTimer } from '@/components/app';
+import { SessionRefreshTimer } from '@/components/app';
+import { useRouter } from 'next/router';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+import NProgress from 'nprogress';
+
+NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const handleRouteStart = () => NProgress.start();
+  const handleRouteDone = () => NProgress.done();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleRouteStart);
+    router.events.on('routeChangeComplete', handleRouteDone);
+    router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteStart);
+      router.events.off('routeChangeComplete', handleRouteDone);
+      router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, []);
+
   const Layout = Component.Layout || HomeLayout;
 
   return (
-    <AppContextProvider>
-      <Layout>
+    <SSRProvider>
+      <Layout {...pageProps}>
         <SessionRefreshTimer />
         <Component {...pageProps} />
       </Layout>
-    </AppContextProvider>
+    </SSRProvider>
   );
 }
 
