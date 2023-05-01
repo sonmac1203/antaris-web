5ff4a518762e0231264eb72b84d8ed28c3aee8e8
Son Mac <mactranthienson@gmail.com>
2023-04-30 23:27:26 -0700

    Update sessionCookie

--- core/auth/sessionCookie.js
+++ core/auth/sessionCookie.js
@@ -1,6 +1,6 @@
 export const sessionCookie = {
   cookieName: 'session',
-  password: process.env.COOKIE_SECRET,
+  password: 'r9WBGOA4j1EFfgFWvD2ndGA9YT114s4Bo2vMTLX05dGWNuN5sPXiWZ6bWYItVOJx',
   cookieOptions: {
     secure: process.env.NODE_ENV === 'production',
   },
